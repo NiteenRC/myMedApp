ec54702338b425f43f912d5cc17308def9913c6d
niteenrc <niteen2010@gmail.com>
2018-10-06 17:58:23 +0530

    Calculation coorected

--- target/classes/static/js/controller/user/cartCtrl.js
+++ target/classes/static/js/controller/user/cartCtrl.js
@@ -33,15 +33,26 @@ function cartController(
     };
 
     $scope.selectedProduct = function(productID) {
+        var originalProductName = null;
         var originalPrice = 0;
+        var originalProductID = 0;
         for (var i = 0; i < $scope.products.length; i++) {
             if ($scope.products[i].productID == productID) {
+                originalProductID = $scope.products[i].productID
                 originalPrice = $scope.products[i].price;
+                originalProductName = $scope.products[i].productName;
             }
         }
         for (var i = 0; i < $scope.rows.length; i++) {
+            if (!$scope.products[i]) {
+                alert('Medicine is already Selected!!');
+                return;
+            }
+
             if ($scope.products[i].productID == productID) {
+                $scope.rows[i].productID = originalProductID
                 $scope.rows[i].price = originalPrice;
+                $scope.rows[i].productName = originalProductName;
             }
         }
     };
@@ -50,8 +61,10 @@ function cartController(
 
     $scope.addRow = function() {
         $scope.rows.push({
-            firstName: $scope.firstName,
-            lastName: $scope.lastName,
+            productID: 0,
+            productName: null,
+            qty: 1,
+            price: 0
         });
     };
 
@@ -88,7 +101,7 @@ function cartController(
     }
 
     function addCartList() {
-        sharedService.postMethod(CART_LIST_SAVE, $scope.carts).then(
+        sharedService.postMethod(CART_LIST_SAVE, $scope.rows).then(
             function(response) {
                 $location.path('/checkout');
                 $scope.carts = response.data;
@@ -113,19 +126,14 @@ function cartController(
         );
     }
 
-    function removeCart(id) {
-        sharedService.deleteMethod(cartUrl + id).then(
-            function(response) {
-                getCarts();
-                getAlCartCount();
-                $scope.successMessage = 'User created successfully';
-                $scope.errorMessage = '';
-            },
-            function(error) {
-                $scope.errorMessage = 'Some thing went wrong' + error;
-                $scope.successMessage = '';
-            }
-        );
+    function removeCart(index) {
+        angular.forEach($scope.rows, function (row) {
+              $scope.rows.splice(index, 1);
+              //if no columns left in the row push a blank array
+              if (row.length === 0) {
+                $scope.rows.data = [];
+              }
+            });
     }
 
     function getAlCartCount() {
@@ -142,9 +150,17 @@ function cartController(
 
     $scope.getTotal = function(val) {
         var total = 0;
-        angular.forEach($scope.carts, function(oldVal) {
+        angular.forEach($scope.rows, function(oldVal) {
             total += oldVal[val];
         });
         return total;
     };
+
+    $scope.totalAmountToPaid = function(){
+        var totalAmount = 0;
+        angular.forEach($scope.rows, function(val) {
+            totalAmount += val.price*val.qty;
+        });
+        return totalAmount;
+    }
 }
