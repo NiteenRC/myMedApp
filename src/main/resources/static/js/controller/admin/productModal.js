aabb735ae70444135e4d8e33209d5be3506b803a
niteenrc <niteen2010@gmail.com>
2018-10-06 16:10:35 +0530

    calculations

--- src/main/resources/static/js/controller/admin/productModal.js
+++ src/main/resources/static/js/controller/admin/productModal.js
@@ -1,55 +1,60 @@
-angular.module('myCart.product_module.productModal',
-		[ 'myCart.shared_module.sharedService' ]).controller(
-		'productModalCtrl', modalCtrl);
+angular
+    .module('myCart.product_module.productModal', [
+        'myCart.shared_module.sharedService',
+    ])
+    .controller('productModalCtrl', modalCtrl);
 
 function modalCtrl($scope, $http, $modalInstance, sharedService) {
-	'use strict';
+    'use strict';
 
-	var productUrl = "/product";
-	$scope.saveProduct = saveProduct;
-	$scope.cancel = cancel;
-	$scope.clear = clear;
-	$scope.categories = sharedService.get('categories');
+    var productUrl = '/product';
+    $scope.saveProduct = saveProduct;
+    $scope.cancel = cancel;
+    $scope.clear = clear;
+    $scope.categories = sharedService.get('categories');
 
-	$scope.productData = {
-		productID : null,
-		productName : null,
-		productDesc : null,
-		price : null
-	};
+    $scope.productData = {
+        productID: null,
+        productName: null,
+        productDesc: null,
+        price: null,
+    };
 
-	function saveProduct() {
+    function saveProduct() {
+        if (!sharedService.isDefinedOrNotNull($scope.productData.productName)) {
+            return alert('Please add Product name');
+        }
 
-		if (!sharedService.isDefinedOrNotNull($scope.productData.productName)) {
-			return alert('Please add Product name')
-		}
+        if (!sharedService.isDefinedOrNotNull($scope.productData.price)) {
+            return alert('Please add price');
+        } else if (isNaN($scope.productData.price)) {
+            return alert('Price should be number');
+        }
 
-		if (!sharedService.isDefinedOrNotNull($scope.productData.price)) {
-			return alert('Please add price')
-		} else if (isNaN($scope.productData.price)) {
-			return alert('Price should be number')
-		}
+        if (!sharedService.isDefinedOrNotNull($scope.item)) {
+            return alert('Please select category');
+        }
 
-		if (!sharedService.isDefinedOrNotNull($scope.item)) {
-			return alert('Please select category')
-		}
+        sharedService
+            .postMethod(
+                productUrl + '/' + $scope.item.categoryID,
+                $scope.productData
+            )
+            .then(
+                function(response) {
+                    $modalInstance.close(response);
+                },
+                function(error) {
+                    alert(error.data.errorMessage);
+                }
+            );
+    }
 
-		//var fd = new FormData();
-		//fd.append('productData', JSON.stringify($scope.productData));
+    function cancel() {
+        $modalInstance.dismiss('cancel');
+    }
 
-		sharedService.postMethod(productUrl + "/" + $scope.item.categoryID, $scope.productData)
-				.then(function(response) {
-					$modalInstance.close(response);
-				}, function(error) {
-					alert(error.data.errorMessage);
-				});
-	}
-
-	function cancel() {
-		$modalInstance.dismiss('cancel');
-	}
-
-	function clear() {
-		$scope.productData = {};
-	}
+    function clear() {
+        $scope.productData = {};
+    }
 }
