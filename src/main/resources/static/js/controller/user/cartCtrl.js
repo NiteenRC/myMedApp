5166e8e2f6068941638aa19105ea28eee1cf4c4c
niteenrc <niteen2010@gmail.com>
2018-10-16 20:06:36 +0530

    delete table row by duplicate product selection

--- src/main/resources/static/js/controller/user/cartCtrl.js
+++ src/main/resources/static/js/controller/user/cartCtrl.js
@@ -1,132 +1,132 @@
 angular
-    .module('myCart.cart_module', ['myCart.shared_module.sharedService'])
-    .controller('cartController', cartController);
+  .module('myCart.cart_module', ['myCart.shared_module.sharedService'])
+  .controller('cartController', cartController);
 
 function cartController(
-    $scope,
-    $rootScope,
-    $uibModal,
-    sharedService,
-    $location
+  $scope,
+  $rootScope,
+  $uibModal,
+  sharedService,
+  $location
 ) {
-    'use strict';
+  'use strict';
 
-    var CART_LIST_SAVE = '/carts';
-    var CART_LIST_DELETE = '/cartsDelete';
-    var productsUrl = '/products';
+  var CART_LIST_SAVE = '/carts';
+  var CART_LIST_DELETE = '/cartsDelete';
+  var productsUrl = '/products';
 
-    fetchAllProducts();
+  $scope.rows = [];
 
-    $scope.removeCart = removeCart;
-    $scope.addCartList = addCartList;
-    $scope.removeCartList = removeCartList;
+  fetchAllProducts();
+  addRow();
+  $scope.addRow = addRow;
 
-    $scope.cartData = {
-        productName: null,
-        price: null,
-        image: null,
-        qty: null,
-    };
+  $scope.removeCart = removeCart;
+  $scope.addCartList = addCartList;
+  $scope.removeCartList = removeCartList;
 
-    $scope.selectedProduct = function(productID, index) {
-        var originalProductName = null;
-        var originalPrice = 0;
-        var originalProductID = 0;
-        for (var i = 0; i < $scope.products.length; i++) {
-            if ($scope.products[i].productID == productID) {
-                originalProductID = $scope.products[i].productID
-                originalPrice = $scope.products[i].price;
-                originalProductName = $scope.products[i].productName;
-            }
-        }
-        for (var i = 0; i < $scope.rows.length; i++) {
-            if (!$scope.products[i]) {
-                alert('Medicine is already Selected!!');
-                removeCart(index);
-                return;
-            }
+  $scope.cartData = {
+    productName: null,
+    price: null,
+    image: null,
+    qty: null,
+  };
 
-            if ($scope.products[i].productID == productID) {
-                $scope.rows[i].productID = originalProductID
-                $scope.rows[i].price = originalPrice;
-                $scope.rows[i].productName = originalProductName;
-            }
-        }
-    };
-
-    $scope.rows = [];
-
-    $scope.addRow = function() {
-        $scope.rows.push({
-            productID: 0,
-            productName: null,
-            qty: 1,
-            price: 0
-        });
-    };
-
-    function fetchAllProducts() {
-        sharedService.getAllMethod(productsUrl).then(
-            function(response) {
-                $scope.products = response.data;
-            },
-            function(error) {
-                $scope.errorMessage = 'Some thing went wrong' + error;
-                $scope.successMessage = '';
-            }
-        );
+  $scope.selectedProduct = function(productID, index) {
+    var originalProductName = null;
+    var originalPrice = 0;
+    var originalProductID = 0;
+    for (var i = 0; i < $scope.products.length; i++) {
+      if ($scope.products[i].productID == productID) {
+        originalProductID = $scope.products[i].productID
+        originalPrice = $scope.products[i].price;
+        originalProductName = $scope.products[i].productName;
+      }
     }
 
-    function addCartList() {
-        sharedService.postMethod(CART_LIST_SAVE, $scope.rows).then(
-            function(response) {
-                $location.path('/checkout');
-                $scope.carts = response.data;
-            },
-            function(error) {
-                $scope.errorMessage = 'Error while creating' + error;
-                $scope.successMessage = '';
-            }
-        );
+    var duplicateCount = 0;
+    for (var i = 0; i < $scope.rows.length; i++) {
+      if ($scope.rows[i].productID == productID) {
+        duplicateCount++;
+      }
+
+      if ((duplicateCount >=1 && index==0) || duplicateCount >= 2) {
+        alert('Medicine is already Selected!!');
+        removeCart(index);
+        return;
+      }
+
+      $scope.rows[index].productID = originalProductID
+      $scope.rows[index].price = originalPrice;
+      $scope.rows[index].productName = originalProductName;
     }
+  };
 
-    function removeCartList(id) {
-    		sharedService.postMethod(CART_LIST_DELETE, $scope.rows).then(
-                        function(response) {
-                            $location.path('/checkout');
-                            $scope.carts = response.data;
-                        },
-                        function(error) {
-                            alert(error.data.errorMessage);
-                            $scope.errorMessage = 'Error while creating' + error;
-                            $scope.successMessage = '';
-                        }
-                    );
-    	}
+  function addRow() {
+    $scope.rows.push({
+      productID: 0,
+      productName: null,
+      qty: 1,
+      price: 0
+    });
+  };
 
-    function removeCart(index) {
-        angular.forEach($scope.rows, function (row) {
-              $scope.rows.splice(index, 1);
-              //if no columns left in the row push a blank array
-              if (row.length === 0) {
-                $scope.rows.data = [];
-              }
-            });
-    }
+  function fetchAllProducts() {
+    sharedService.getAllMethod(productsUrl).then(
+      function(response) {
+        $scope.products = response.data;
+      },
+      function(error) {
+        $scope.errorMessage = 'Some thing went wrong' + error;
+        $scope.successMessage = '';
+      }
+    );
+  }
 
-    $scope.getTotal = function(val) {
-        var total = 0;
-        angular.forEach($scope.rows, function(oldVal) {
-            total += oldVal[val];
-        });
-        return total;
-    };
+  function addCartList() {
+    sharedService.postMethod(CART_LIST_SAVE, $scope.rows).then(
+      function(response) {
+        $scope.carts = response.data;
+        $scope.rows = [];
+      },
+      function(error) {
+        $scope.errorMessage = 'Error while creating' + error;
+        $scope.successMessage = '';
+      }
+    );
+  }
 
-    $scope.totalAmountToPaid = function(){
-        var totalAmount = 0;
-        angular.forEach($scope.rows, function(val) {
-            totalAmount += val.price*val.qty;
-        });
-        return totalAmount;
-    }
+  function removeCartList(id) {
+    sharedService.postMethod(CART_LIST_DELETE, $scope.rows).then(
+      function(response) {
+        $scope.carts = response.data;
+        $scope.rows = [];
+      },
+      function(error) {
+        alert(error.data.errorMessage);
+        $scope.errorMessage = 'Error while creating' + error;
+        $scope.successMessage = '';
+      }
+    );
+  }
+
+  function removeCart(index) {
+      $scope.rows.splice(index, 1);
+  }
+
+  $scope.getTotal = function(val) {
+    var total = 0;
+    angular.forEach($scope.rows, function(oldVal) {
+      total += oldVal[val];
+    });
+    return total;
+  };
+
+  $scope.totalAmountToPaid = function() {
+    var totalAmount = 0;
+    angular.forEach($scope.rows, function(val) {
+      totalAmount += val.price * val.qty;
+    });
+    return totalAmount;
+  }
 }
